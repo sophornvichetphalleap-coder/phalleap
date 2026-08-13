b9c62ab0c722df75a06955e7db0a1b8eca7f6322
sophornvichetphalleap-coder <sophornvichetphalleap@gmail.com>
2026-08-13 12:05:08 +0700

    Remove script tag and menu toggle logic

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<script>
-    const menuBtn = document.getElementById('mobile-menu-btn');
-    const navMenu = document.getElementById('nav-menu');
 
-    if (menuBtn && navMenu) {
-        menuBtn.addEventListener('click', () => {
-            navMenu.classList.toggle('active');
-        });
-    }
-</script>
